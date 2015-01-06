7bf5ce5ee4430dee005cd45dad1093b97b073629
Der-Johng Sun <derjohng@doitwell.tw>
2015-01-06 09:48:13 +0800

    backup.

--- Classes/JCStockGraphView.m
+++ Classes/JCStockGraphView.m
@@ -197,7 +197,7 @@
     rangeLabel.backgroundColor  = [UIColor colorWithWhite:0.0 alpha:0.4];
     [rangeLabel sizeToFit];
     rangeLabel.width            += 8;
-    rangeLabel.y                = self.height; // - rangeLabel.height;
+    rangeLabel.y                = self.height + 4; // - rangeLabel.height;
     rangeLabel.right            = self.right;
     rangeLabel.autoresizingMask = UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleLeftMargin;
     [self addSubview:rangeLabel];
